00404cb1048c5417ea0b78ff10e0e6639b23e5c7
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2026-01-03 19:07:11 +0300

    start youtube project

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ButtonList from "./ButtonList";
+import VideoConianer from "./VideoConianer";
+
+const MainContainer = () => {
+  return (
+    <div>
+      <ButtonList />
+      <VideoConianer />
+    </div>
+  );
+};
+
+export default MainContainer;
---
+++ src/components/VideoConianer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoConianer = () => {
+  return <div>VideoConianer</div>;
+};
+
+export default VideoConianer;
